abdb083317523fac0469a9c7ed36fde337c42f42
willa kiane <willa_abreu@outlook.com>
2020-01-13 23:12:09 -0300

    estilos formulario e lib uniform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>Sign In</h1>;
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <h1>Sign Up</h1>;
+  function handleSubmit(data) {
+    console.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Seu nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
