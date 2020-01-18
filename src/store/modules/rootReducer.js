9e3987556d8bdbbd852bd5e5d7e9696a2153856a
willa <wkiane@pop-os.localdomain>
2020-01-18 00:46:20 -0300

    armazenando perfil

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
-import auth from './auth/reducer';
 
-export default combineReducers({
+import auth from './auth/reducer';
+import user from './user/reducer';
+
+const reducers = combineReducers({
   auth,
+  user,
 });
+
+export default reducers;
